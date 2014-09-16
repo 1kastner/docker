41304678b80aa3f512457190262e35bfa488b8e0
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-09-16 08:25:47 +0000

    Changed to use config file instead of cli parameters

--- ais-abnormal-analyzer/start.sh
+++ ais-abnormal-analyzer/start.sh
@@ -1,3 +1,3 @@
 cd /AisAbnormal/ais-ab-analyzer/target/ais-ab-analyzer-0.1-SNAPSHOT
-./run-analyzer.sh $DB_HOST $DB_PORT $DB_USER $DB_PASS $DB_INST $STAT_FILE $AIS_SOURCE
+./run-analyzer.sh $CONFIG_FILE
 
