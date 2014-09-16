01a930ac639abda1a2cb287953b0d613cebcf6bb
Thomas Borg Salling <tbsalling@tbsalling.dk>
2014-09-16 10:59:42 +0000

    Docker image run configurable git commit

--- ais-abnormal-analyzer/start.sh
+++ ais-abnormal-analyzer/start.sh
@@ -1,3 +1,9 @@
-cd /AisAbnormal/ais-ab-analyzer/target/ais-ab-analyzer-0.1-SNAPSHOT
+cd /AisAbnormal
+git fetch --all
+git checkout $GIT_COMMIT
+mvn clean install -DskipTests 
+cd /AisAbnormal/ais-ab-analyzer/target/
+unzip ais-ab-analyzer-0.1-SNAPSHOT-bundle.zip
+cd ais-ab-analyzer-0.1-SNAPSHOT
 ./run-analyzer.sh $CONFIG_FILE
 
